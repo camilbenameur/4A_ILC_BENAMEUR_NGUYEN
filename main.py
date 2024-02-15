0469f7ddcdcf4b8ee8e2772c33a33c4070f290ba
Camil Benameur <103584333+camilbenameur@users.noreply.github.com>
2024-02-15 15:57:41 +0000

    get method handling

--- main.py
+++ main.py
@@ -2,23 +2,24 @@ from flask import Flask, request, jsonify
 import csv
 from datetime import datetime, timedelta
 import redis
+import json
 
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-# Ajout de variable
-r.set('0', '2')                 # Retourne True si réussite
+app = Flask(__name__)
 
-# Lecture de 
-value = r.get('0')                        # Retourne la value de foo
-print(value)
+events_db = []
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
 
-# app = Flask(__name__)
-
-
-# events_db = []
-
-# if __name__ == '__main__':
-#     app.run(debug=True, port=5000)
-
+@app.route('/operation', methods=['GET'])
+def get_result_from_id():
+    id = request.args.get('id')
+    operation_result = r.get(id)
+    if operation_result:
+        return operation_result
+    else:
+        return "Event not found", 404
+    
