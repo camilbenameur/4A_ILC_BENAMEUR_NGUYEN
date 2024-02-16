531198145e59e9fcd69113b9e18fe8071d04407e
LionelNG <119571643+LionelKhoi@users.noreply.github.com>
2024-02-16 09:59:10 +0000

    redis

--- main.py
+++ main.py
@@ -1,29 +1,18 @@
 from flask import Flask, request, jsonify
-import csv
 import redis
 import uuid
-from datetime import datetime, timedelta
-import redis
 import json
 
-
-r = redis.Redis(host='localhost', port=6379, db=0)
-
 app = Flask(__name__)
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
-
-
+r = redis.Redis(host='localhost', port=6379, db=0)
 
 @app.route('/operation', methods=['POST'])
 def operation():
     data = request.get_json()
     
-    first_nb = float(data[0])
-    operation = data[1]
-    second_nb = float(data[2])
+    first_nb = float(data.get("0"))
+    operation = data.get("1")
+    second_nb = float(data.get("2"))
 
     if operation == "+":
         result = first_nb + second_nb
@@ -37,11 +26,8 @@ def operation():
     random_uuid = uuid.uuid4()
     r.set(str(random_uuid), result)
     
-    return jsonify({random_uuid}), 201
+    return jsonify({"id": str(random_uuid)}), 201  # Convert UUID to string before returning in JSON
 
-
-
-# Nouvelle route pour la racine ("/")
 @app.route('/')
 def home():
     return "Bienvenue !"
@@ -51,8 +37,9 @@ def get_result_from_id():
     id = request.args.get('id')
     operation_result = r.get(id)
     if operation_result:
-        return operation_result
+        return jsonify({"result": float(operation_result)})  # Ensure result is converted to float before returning
     else:
         return "Event not found", 404
-    
 
+if __name__ == '__main__':
+    app.run(debug=True, port=5000)
